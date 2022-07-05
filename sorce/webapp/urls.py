ace080a84322f9913ae842775e63d7cd3ae528b7
Zakawww <zaka_333@mail.ru>
2022-07-05 10:55:33 +0600

    add base

---
+++ sorce/webapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from webapp.views import index, delete,detail,create,update,filter_by_category,search
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('product/<int:pk>', detail, name='detail'),
+    path('create/', create, name='create'),
+    path('update/<int:pk>', update, name='update'),
+    path('delete/<int:pk>', delete, name='delete'),
+    path('products/<str:category>', filter_by_category, name='category'),
+    path('search/', search, name='search'),
+]
