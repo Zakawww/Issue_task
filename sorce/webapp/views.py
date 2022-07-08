0ca30979075df69028cc0dab49446b882412ec51
Zakawww <zaka_333@mail.ru>
2022-07-08 16:04:30 +0600

    refactor view.py

--- sorce/webapp/views.py
+++ sorce/webapp/views.py
@@ -1,41 +1,50 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView, View
+
 from .forms import SearchForm, IssueForm
 from .models import Issue
 
 
-# def get_categories():
-#     products = Issue.objects.exclude(count=0)
-#     categories = []
-#     for product in products:
-#         category = str(product).split('-')
-#         if category[1].strip() not in categories:
-#             categories.append(category[1].strip())
-#     return categories
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = SearchForm()
+        issues = Issue.objects.all()
+        context['issues'] = issues
+        context['form'] = form
+        return context
 
 
-def index(request):
-    form = SearchForm()
-    issues = Issue.objects.order_by('summary')
-    return render(request, 'index.html', {'issues': issues, 'form': form})
+class DetailView(TemplateView):
+    template_name = 'detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = kwargs.get("pk")
+        issues = get_object_or_404(Issue, pk=pk)
+        context['issues'] = issues
+        return context
 
 
-def detail(request, pk):
-    issues = get_object_or_404(Issue, pk=pk)
-    return render(request, 'detail.html', {'issues': issues})
+class DeleteView(View):
+    def get(self, request, pk):
+        issue = get_object_or_404(Issue, pk=pk)
+        return render(request, 'index.html', {'issue': issue})
+
+    def post(self, request, pk):
+        issue = get_object_or_404(Issue, pk=pk)
+        issue.delete()
+        return redirect('index')
 
 
-def delete(request, pk):
-    issues = get_object_or_404(Issue, pk=pk)
-    issues.delete()
-    return redirect('index')
-
-
-def create(request):
-    if request.method == 'GET':
+class CreateView(View):
+    def get(self, request, *args, **kwargs):
         form = IssueForm()
         return render(request, 'create.html', {'form': form})
 
-    elif request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         form = IssueForm(data=request.POST)
         if form.is_valid():
             Issue.objects.create(
@@ -46,46 +55,41 @@ def create(request):
             )
             return redirect('index')
         else:
-            return render(request, 'create.html', context={'form': form})
+            return render(request, 'create.html', {'form': form})
 
 
-def update(request, pk):
-    issues = get_object_or_404(Issue, pk=pk)
-
-    if request.method == 'GET':
+class UpdateView(View):
+    def get(self, request, *args, **kwargs):
+        issue_pk = kwargs.get('pk')
+        issue = get_object_or_404(Issue, pk=issue_pk)
         form = IssueForm(data={
-            'summary': issues.summary,
-            'description': issues.description,
-            'status': issues.status,
-            'type': issues.type
+            'summary': issue.summary,
+            'description': issue.description,
+            'status': issue.status.id,
+            'type': issue.type.id
         })
-        return render(request, 'update.html', context={'form': form, 'issues': issues})
+        return render(request, 'update.html', {'form': form, 'issue': issue})
 
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         form = IssueForm(data=request.POST)
+        issue_pk = kwargs.get('pk')
+        issue = get_object_or_404(Issue, pk=issue_pk)
         if form.is_valid():
-            issues.summary = form.cleaned_data['summary']
-            issues.description = form.cleaned_data['description']
-            issues.status = form.cleaned_data['status']
-            issues.type = form.cleaned_data['type']
-            issues.save()
+            issue.summary = form.cleaned_data['summary']
+            issue.description = form.cleaned_data['description']
+            issue.status = form.cleaned_data['status']
+            issue.type = form.cleaned_data['type']
+            issue.save()
             return redirect('index')
         else:
-            return render(request, 'update.html', context={'form': form})
-
-#
-# def filter_by_category(request, category):
-#     products = Issue.objects.filter(category=category).exclude(count=0).order_by('name')
-#     # categories = get_categories()
-#     return render(request, 'filter_category.html',
-#                   {'products': products, 'categories': categories, 'category': category})
+            return render(request, 'update.html', {'form': form, 'issue': issue})
 
 
 def search(request):
     form = SearchForm(data=request.GET)
     if form.is_valid():
         summary = form.cleaned_data['summary']
-        issues = Issue.objects.filter(name__contains=summary)
+        issues = Issue.objects.filter(summary__contains=summary)
         return render(request, 'index.html', {'issues': issues, 'form': form})
     else:
         return redirect('index')
