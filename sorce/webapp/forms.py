da204fdbb004ac9205f55a5c3ef3525bdcdef9ee
Zakawww <zaka_333@mail.ru>
2022-07-08 13:39:49 +0600

    refactor choice

--- sorce/webapp/forms.py
+++ sorce/webapp/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from django.forms import widgets
-# from .models import category_choices
+from webapp.models import Status, Type
 
 
-class ProductForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label='Name')
-    description = forms.CharField(max_length=2000, required=False, widget=widgets.Textarea, label='Description')
-    # category = forms.ChoiceField(required=True, choices=category_choices, label='Category')
-    count = forms.IntegerField(required=True, min_value=0, label='Count')
-    price = forms.DecimalField(required=True, max_digits=7, decimal_places=2, label='Price')
+class IssueForm(forms.Form):
+    summary = forms.CharField(max_length=100, required=True, label='summary')
+    description = forms.CharField(max_length=2000, required=False, widget=widgets.Textarea, label='description')
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=False, label='status')
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=False, label='type')
+
 
 
 
 class SearchForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label='Name')
+    summary = forms.CharField(max_length=100, required=True, label='summary')
