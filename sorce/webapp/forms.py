df42a54ecd9011fcca0a41b32357462a59e5589f
Zakawww <zaka_333@mail.ru>
2022-07-08 16:13:25 +0600

    refactor forms.py and view_products.html

--- sorce/webapp/forms.py
+++ sorce/webapp/forms.py
@@ -6,8 +6,8 @@ from webapp.models import Status, Type
 class IssueForm(forms.Form):
     summary = forms.CharField(max_length=100, required=True, label='summary')
     description = forms.CharField(max_length=2000, required=False, widget=widgets.Textarea, label='description')
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='status')
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=True, label='type')
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), empty_label=None, required=True, label='status')
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), empty_label=None, required=True, label='type')
 
 
 class SearchForm(forms.Form):
