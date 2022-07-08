a34125be755330e2739ce41f51be7ef454320035
Zakawww <zaka_333@mail.ru>
2022-07-08 13:40:06 +0600

    refactor urls

--- sorce/webapp/urls.py
+++ sorce/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from webapp.views import index, delete, detail, create, update, filter_by_category, search
+from webapp.views import index, delete, detail, create, update, search
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,6 +8,5 @@ urlpatterns = [
     path('create/', create, name='create'),
     path('update/<int:pk>', update, name='update'),
     path('delete/<int:pk>', delete, name='delete'),
-    path('products/<str:category>', filter_by_category, name='category'),
     path('search/', search, name='search'),
 ]
--- sorce/webapp/views.py
+++ sorce/webapp/views.py
@@ -1,51 +1,49 @@
 from django.shortcuts import render, get_object_or_404, redirect
-# from .models import Product, category_choices
-from .forms import ProductForm, SearchForm
+from .forms import SearchForm, IssueForm
 from .models import Issue
 
 
-def get_categories():
-    products = Issue.objects.exclude(count=0)
-    categories = []
-    for product in products:
-        category = str(product).split('-')
-        if category[1].strip() not in categories:
-            categories.append(category[1].strip())
-    return categories
+# def get_categories():
+#     products = Issue.objects.exclude(count=0)
+#     categories = []
+#     for product in products:
+#         category = str(product).split('-')
+#         if category[1].strip() not in categories:
+#             categories.append(category[1].strip())
+#     return categories
 
 
 def index(request):
     form = SearchForm()
-    products = Issue.objects.order_by('category', 'name').exclude(count=0)
-    categories = get_categories()
-    return render(request, 'index.html', {'products': products, 'categories': categories, 'form': form})
+    issues = Issue.objects.order_by('summary')
+    # categories = get_categories()
+    return render(request, 'index.html', {'issues': issues, 'form': form})
 
 
 def detail(request, pk):
-    product = get_object_or_404(Issue, pk=pk)
-    return render(request, 'detail.html', {'product': product})
+    issues = get_object_or_404(Issue, pk=pk)
+    return render(request, 'detail.html', {'issues': issues})
 
 
 def delete(request, pk):
-    product = get_object_or_404(Issue, pk=pk)
-    product.delete()
+    issues = get_object_or_404(Issue, pk=pk)
+    issues.delete()
     return redirect('index')
 
 
 def create(request):
     if request.method == 'GET':
-        form = ProductForm()
-        return render(request, 'create.html', {'statuses': 'status', 'form': form})
+        form = IssueForm()
+        return render(request, 'create.html', {'form': form})
 
     elif request.method == 'POST':
-        form = ProductForm(data=request.POST)
+        form = IssueForm(data=request.POST)
         if form.is_valid():
             Issue.objects.create(
-                name=form.cleaned_data['name'],
+                summary=form.cleaned_data['summary'],
                 description=form.cleaned_data['description'],
-                category=form.cleaned_data['category'],
-                count=form.cleaned_data['count'],
-                price=form.cleaned_data['price']
+                status=form.cleaned_data['status'],
+                type=form.cleaned_data['type']
             )
             return redirect('index')
         else:
@@ -53,45 +51,43 @@ def create(request):
 
 
 def update(request, pk):
-    product = get_object_or_404(Issue, pk=pk)
+    issues = get_object_or_404(Issue, pk=pk)
 
     if request.method == 'GET':
-        form = ProductForm(data={
-            'name': product.name,
-            'description': product.description,
-            'category': product.category,
-            'count': product.count,
-            'price': product.price
+        form = IssueForm(data={
+            'summary': issues.summary,
+            'description': issues.description,
+            'status': issues.category,
+            'type': issues.count
         })
-        return render(request, 'update.html', context={'form': form, 'product': product})
+        return render(request, 'update.html', context={'form': form, 'issues': issues})
 
     if request.method == 'POST':
-        form = ProductForm(data=request.POST)
+        form = IssueForm(data=request.POST)
         if form.is_valid():
-            product.name = form.cleaned_data['name']
-            product.description = form.cleaned_data['description']
-            product.category = form.cleaned_data['category']
-            product.count = form.cleaned_data['count']
-            product.price = form.cleaned_data['price']
-            product.save()
+            issues.summary = form.cleaned_data['summary']
+            issues.description = form.cleaned_data['description']
+            issues.status = form.cleaned_data['status']
+            issues.type = form.cleaned_data['type']
+            issues.save()
             return redirect('index')
         else:
             return render(request, 'update.html', context={'form': form})
 
-
-def filter_by_category(request, category):
-    products = Issue.objects.filter(category=category).exclude(count=0).order_by('name')
-    categories = get_categories()
-    return render(request, 'filter_category.html',
-                  {'products': products, 'categories': categories, 'category': category})
+#
+# def filter_by_category(request, category):
+#     products = Issue.objects.filter(category=category).exclude(count=0).order_by('name')
+#     # categories = get_categories()
+#     return render(request, 'filter_category.html',
+#                   {'products': products, 'categories': categories, 'category': category})
 
 
 def search(request):
-    categories = get_categories()
+    # categories = get_categories()
     form = SearchForm(data=request.GET)
     if form.is_valid():
-        name = form.cleaned_data['name']
-        products = Issue.objects.filter(name__contains=name).exclude(count=0)
-        return render(request, 'index.html', {'products': products, 'categories': categories, 'form': form})
+        summary = form.cleaned_data['summary']
+        issues = Issue.objects.filter(name__contains=summary)
+        return render(request, 'index.html', {'issues': issues, 'form': form})
     else:
         return redirect('index')
