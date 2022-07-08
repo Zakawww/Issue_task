0e3e695d04ef8841450e8ef32d8370c630ee7741
Zakawww <zaka_333@mail.ru>
2022-07-08 16:04:51 +0600

    refactor urls.py

--- sorce/webapp/forms.py
+++ sorce/webapp/forms.py
@@ -6,11 +6,9 @@ from webapp.models import Status, Type
 class IssueForm(forms.Form):
     summary = forms.CharField(max_length=100, required=True, label='summary')
     description = forms.CharField(max_length=2000, required=False, widget=widgets.Textarea, label='description')
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=False, label='status')
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=False, label='type')
-
-
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='status')
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=True, label='type')
 
 
 class SearchForm(forms.Form):
-    summary = forms.CharField(max_length=100, required=True, label='summary')
+    summary = forms.CharField(max_length=60, required=False, label='summary')
--- sorce/webapp/urls.py
+++ sorce/webapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from webapp.views import index, delete, detail, create, update, search
+from webapp.views import IndexView, DetailView, CreateView, DeleteView, UpdateView, search
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('product/<int:pk>', detail, name='detail'),
-    path('create/', create, name='create'),
-    path('update/<int:pk>', update, name='update'),
-    path('delete/<int:pk>', delete, name='delete'),
+    path('', IndexView.as_view(), name='index'),
+    path('product/<int:pk>', DetailView.as_view(), name='detail'),
+    path('delete/<int:pk>', DeleteView.as_view(), name='delete'),
+    path('update/<int:pk>', UpdateView.as_view(), name='update'),
+    path('create/', CreateView.as_view(), name='create'),
     path('search/', search, name='search'),
 ]
