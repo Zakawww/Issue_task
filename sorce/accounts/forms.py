b03f8ff878ee23a8b1ef06366faff46b414007dc
Zakawww <zaka_333@mail.ru>
2022-08-09 14:02:46 +0600

    добавил обязательным к заполнению email

--- sorce/accounts/forms.py
+++ sorce/accounts/forms.py
@@ -2,7 +2,6 @@ from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth.models import User
 from django import forms
 
-
 # class MyUserCreationForm(UserCreationForm):
 #     class Meta:
 #         model = User
@@ -16,6 +15,8 @@ class MyUserCreationForm(forms.ModelForm):
     password = forms.CharField(label="Пароль", strip=False, required=True, widget=forms.PasswordInput)
     password_confirm = forms.CharField(label="Подтвердите пароль", required=True, widget=forms.PasswordInput,
                                        strip=False)
+    email = forms.EmailField(max_length=50, widget=forms.EmailInput(attrs={'placeholder': 'from@example.com'}),
+                             label="Email")
 
     def clean(self):
         cleaned_data = super().clean()
@@ -23,13 +24,6 @@ class MyUserCreationForm(forms.ModelForm):
         password_confirm = cleaned_data.get("password_confirm")
         if password and password_confirm and password != password_confirm:
             raise forms.ValidationError('Пароли не совпадают!')
-
-    # def clean_email(self):
-    #     email = self.cleaned_data['email']
-    #     validator = RegexValidator(
-    #         "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-    #     validator(email)
-    #     return email
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -41,7 +35,3 @@ class MyUserCreationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
-
-
-
-
