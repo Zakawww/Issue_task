75199dd6e4c5c560a8876c59ee8f616452adef2a
Zakawww <zaka_333@mail.ru>
2022-08-16 14:53:24 +0600

    add url and about

--- sorce/accounts/models.py
+++ sorce/accounts/models.py
@@ -6,5 +6,7 @@ from django.db import models
 class Profile(models.Model):
     birthday = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
     avatar = models.ImageField(upload_to='avatars', null=True, blank=True, verbose_name='Аватар')
+    url_user = models.URLField(max_length=200, null=True, blank=True, verbose_name='Ссылка')
+    about_user = models.TextField(max_length=1000, null=True, blank=True, verbose_name='О себе')
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, verbose_name='Пользователь',
                                 related_name='profile')
