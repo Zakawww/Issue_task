5b9dba3bda633e9e5d424eebdc0b5554373eaefa
Zakawww <zaka_333@mail.ru>
2022-08-16 17:44:13 +0600

    add

--- sorce/accounts/views.py
+++ sorce/accounts/views.py
@@ -8,6 +8,7 @@ from django.views.generic import CreateView, DetailView
 
 from accounts.forms import MyUserCreationForm
 from accounts.models import Profile
+from webapp.models import Issue
 
 
 def login_view(request):
@@ -62,26 +63,14 @@ class ProfileView(LoginRequiredMixin, DetailView):
         page = paginator.get_page(page_number)
         kwargs['page_obj'] = page
         kwargs['issues'] = page.object_list
+        kwargs['projects'] = page.object_list
         kwargs['is_paginated'] = page.has_other_pages()
         return super().get_context_data(**kwargs)
 
 
-# class UserDetailView(LoginRequiredMixin, DetailView):
-#     model = get_user_model()
-#     template_name = 'user_detail.html'
-#     context_object_name = 'user_obj'
-#     paginate_related_by = 5
-#     paginate_related_orphans = 0
-#
-#     def get_context_data(self, **kwargs):
-#         articles = self.object.articles.order_by('-created_at')
-#         paginator = Paginator(articles, self.paginate_related_by, orphans=self.paginate_related_orphans)
-#         page_number = self.request.GET.get('page', 1)
-#         page = paginator.get_page(page_number)
-#         kwargs['page_obj'] = page
-#         kwargs['articles'] = page.object_list
-#         kwargs['is_paginated'] = page.has_other_pages()
-#         return super().get_context_data(**kwargs)
+
+
+
 
 
 # def register_view(request, *args, **kwargs):
